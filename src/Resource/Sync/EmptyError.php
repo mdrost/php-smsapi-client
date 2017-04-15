955bb3ff8d6e24ffdcd55069d7fdc97c2b35b38c
Mateusz Drost <mat.drost@gmail.com>
2017-04-15 09:44:58 +0200

    add missing resources

---
+++ src/Resource/Sync/EmptyError.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Smsapi\Client\Resource\Sync;
+
+use Smsapi\Client\Resource\EmptyError as BaseEmptyError;
+
+class EmptyError extends BaseEmptyError
+{
+}
---
+++ src/Resource/Sync/Error.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Smsapi\Client\Resource\Sync;
+
+use Smsapi\Client\Resource\Error as BaseError;
+
+class Error extends BaseError
+{
+}
